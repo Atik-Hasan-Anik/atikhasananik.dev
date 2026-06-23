2977ab199ffbfdd80cde82fcd76419a2a8ca4903
Atik-Hasan-Anik <itsatikhasananik@gmail.com>
2026-06-24 00:31:44 +0600

    feture/icon set in the page2

--- src/components/page/page2/Page2.jsx
+++ src/components/page/page2/Page2.jsx
@@ -7,7 +7,7 @@ const Page2 = () => {
     
     {
       sklilName: "Frontend Development",
-      skillIcon: "",
+      skillIcon: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIiB2aWV3Qm94PSIwIDAgMjQgMjQiIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzUwQTJGRiIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNvZGUteG1sLWljb24gbHVjaWRlLWNvZGUteG1sIj48cGF0aCBkPSJtMTggMTYgNC00LTQtNCIvPjxwYXRoIGQ9Im02IDgtNCA0IDQgNCIvPjxwYXRoIGQ9Im0xNC41IDQtNSAxNiIvPjwvc3ZnPg==",
       iconDts: [
         {
           icon: "",
@@ -18,7 +18,7 @@ const Page2 = () => {
 
     {
       sklilName: "Backend Development",
-      skillIcon: "",
+      skillIcon: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIiB2aWV3Qm94PSIwIDAgMjQgMjQiIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzA1REY3MiIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWRhdGFiYXNlLWljb24gbHVjaWRlLWRhdGFiYXNlIj48ZWxsaXBzZSBjeD0iMTIiIGN5PSI1IiByeD0iOSIgcnk9IjMiLz48cGF0aCBkPSJNMyA1VjE5QTkgMyAwIDAgMCAyMSAxOVY1Ii8+PHBhdGggZD0iTTMgMTJBOSAzIDAgMCAwIDIxIDEyIi8+PC9zdmc+",
       iconDts: [
         {
           icon: "",
@@ -29,7 +29,7 @@ const Page2 = () => {
 
     {
       sklilName: "UI/UX Design",
-      skillIcon: "",
+      skillIcon: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIiB2aWV3Qm94PSIwIDAgMjQgMjQiIGZpbGw9Im5vbmUiIHN0cm9rZT0iI0MyN0FGRiIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXBhbmVscy10b3AtbGVmdC1pY29uIGx1Y2lkZS1wYW5lbHMtdG9wLWxlZnQiPjxyZWN0IHdpZHRoPSIxOCIgaGVpZ2h0PSIxOCIgeD0iMyIgeT0iMyIgcng9IjIiLz48cGF0aCBkPSJNMyA5aDE4Ii8+PHBhdGggZD0iTTkgMjFWOSIvPjwvc3ZnPg==",
       iconDts: [
         {
           icon: "",
@@ -40,7 +40,7 @@ const Page2 = () => {
 
     {
       sklilName: "Cloud & DevOps",
-      skillIcon: "",
+      skillIcon: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIiB2aWV3Qm94PSIwIDAgMjQgMjQiIGZpbGw9Im5vbmUiIHN0cm9rZT0iI0ZGODkwNCIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNsb3VkLWljb24gbHVjaWRlLWNsb3VkIj48cGF0aCBkPSJNMTcuNSAxOUg5YTcgNyAwIDEgMSA2LjcxLTloMS43OWE0LjUgNC41IDAgMSAxIDAgOVoiLz48L3N2Zz4=",
       iconDts: [
         {
           icon: "",
@@ -51,7 +51,7 @@ const Page2 = () => {
 
     {
       sklilName: "Tools & Technologies",
-      skillIcon: "",
+      skillIcon: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIiB2aWV3Qm94PSIwIDAgMjQgMjQiIGZpbGw9Im5vbmUiIHN0cm9rZT0iI0ZCNjRCNiIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNwdS1pY29uIGx1Y2lkZS1jcHUiPjxwYXRoIGQ9Ik0xMiAyMHYyIi8+PHBhdGggZD0iTTEyIDJ2MiIvPjxwYXRoIGQ9Ik0xNyAyMHYyIi8+PHBhdGggZD0iTTE3IDJ2MiIvPjxwYXRoIGQ9Ik0yIDEyaDIiLz48cGF0aCBkPSJNMiAxN2gyIi8+PHBhdGggZD0iTTIgN2gyIi8+PHBhdGggZD0iTTIwIDEyaDIiLz48cGF0aCBkPSJNMjAgMTdoMiIvPjxwYXRoIGQ9Ik0yMCA3aDIiLz48cGF0aCBkPSJNNyAyMHYyIi8+PHBhdGggZD0iTTcgMnYyIi8+PHJlY3QgeD0iNCIgeT0iNCIgd2lkdGg9IjE2IiBoZWlnaHQ9IjE2IiByeD0iMiIvPjxyZWN0IHg9IjgiIHk9IjgiIHdpZHRoPSI4IiBoZWlnaHQ9IjgiIHJ4PSIxIi8+PC9zdmc+",
       iconDts: [
         {
           icon: "",
@@ -62,7 +62,7 @@ const Page2 = () => {
 
     {
       sklilName: "Others",
-      skillIcon: "",
+      skillIcon: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIiB2aWV3Qm94PSIwIDAgMjQgMjQiIGZpbGw9Im5vbmUiIHN0cm9rZT0iI0ZEQzcwMCIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWxheW91dC1kYXNoYm9hcmQtaWNvbiBsdWNpZGUtbGF5b3V0LWRhc2hib2FyZCI+PHJlY3Qgd2lkdGg9IjciIGhlaWdodD0iOSIgeD0iMyIgeT0iMyIgcng9IjEiLz48cmVjdCB3aWR0aD0iNyIgaGVpZ2h0PSI1IiB4PSIxNCIgeT0iMyIgcng9IjEiLz48cmVjdCB3aWR0aD0iNyIgaGVpZ2h0PSI5IiB4PSIxNCIgeT0iMTIiIHJ4PSIxIi8+PHJlY3Qgd2lkdGg9IjciIGhlaWdodD0iNSIgeD0iMyIgeT0iMTYiIHJ4PSIxIi8+PC9zdmc+",
       iconDts: [
         {
           icon: "",
@@ -79,7 +79,7 @@ const Page2 = () => {
       </div>
       <div className="grid grid-cols-3 self-center justify-self-center gap-5 mt-8 ">
         {skills.map((elem ,idx) => {
-          return <SkillsCard id = {idx} cardHeading={elem.sklilName} />;
+          return <SkillsCard skillIcon={elem.skillIcon} id = {idx} cardHeading={elem.sklilName} />;
         })}
       </div>
     </section>
--- src/components/page/page2/SkillsCard.jsx
+++ src/components/page/page2/SkillsCard.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import SkillBtn from "../../comon button/SkillBtn";
 
 const SkillsCard = (props) => {
-
+  
   return (
-    <div id={`${props.id}`} className="h-55 w-110 hovering  rounded-2xl border border-gray-600 p-7 bg-[#0E1527] ">
+    <div
+      id={`${props.id}`}
+      className="h-55 w-110 hovering  rounded-2xl border border-gray-600 p-7 bg-[#0E1527] "
+    >
       <div className="flex items-center gap-4">
-        <div className="text-2xl bg-[#2a76c3] px-4 py-4 h-10 w-10 scile rounded flex items-center justify-center ">o</div>
-        <h2 className="text-3xl font-bold text-white  ">
-          {props.cardHeading}
-        </h2>
+        <div className="text-2xl bg-[#161F30] px-2 py-2  scile transition-all duration-300 rounded-xl flex items-center justify-center ">
+
+          <img className="h-8 w-8 font-bold text-7xl" src={props.skillIcon} alt="icon" />
+
+        </div>
+
+        <h2 className="text-3xl font-bold text-white  ">{props.cardHeading}</h2>
+
       </div>
       <div className="pt-5  flex gap-2  flex-wrap">
         <SkillBtn />
